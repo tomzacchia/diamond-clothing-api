070f981f1a203071d70e8c4dea9830cd4636fe57
tomzacchia <tomzacchia@live.com>
2020-02-19 12:40:37 -0500

    Add reponse to post request

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+// return stripe object
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -20,4 +23,20 @@ app.use(cors());
 app.listen(port, error => {
   if (error) throw error;
   console.log(`Server running on port ${port}`);
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "cad"
+  };
+
+  stripe.charges.create(body, (stripeError, stripeResponse) => {
+    if (stripeError) {
+      res.status(500).send({ error: stripeError });
+    } else {
+      res.status(200).send({ success: stripeResponse });
+    }
+  });
 });
